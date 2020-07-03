2018adfc89d1247ac4cbc2863f29c28bec07d3e6
Marc Dos Santos <mdossantosdev@gmail.com>
2020-07-03 11:52:07 +0200

    Create Register screen

---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Title, IconButton } from 'react-native-paper';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
+
+export default function RegisterScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Title style={styles.titleText}>Register to chat</Title>
+      <FormInput
+        labelName='Email'
+        value={email}
+        autoCapitalize='none'
+        onChangeText={(userEmail) => setEmail(userEmail)}
+      />
+      <FormInput
+        labelName='Password'
+        value={password}
+        secureTextEntry
+        onChangeText={(userPassword) => setPassword(userPassword)}
+      />
+      <FormButton
+        title='Register'
+        modeValue='contained'
+        labelStyle={styles.registerButtonLabel}
+      />
+      <IconButton
+        icon='keyboard-backspace'
+        size={30}
+        color='#6646ee'
+        style={styles.navButton}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  titleText: {
+    fontSize: 22,
+    marginBottom: 10,
+  },
+  registerButtonLabel: {
+    fontSize: 20,
+  },
+  navButton: {
+    marginTop: 10,
+  },
+});
