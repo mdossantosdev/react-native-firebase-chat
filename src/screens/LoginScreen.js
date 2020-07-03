edce4468287810905277f528deca5ee6f9870e12
Marc Dos Santos <mdossantosdev@gmail.com>
2020-07-03 11:16:31 +0200

    Create Login screen

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Title } from 'react-native-paper';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Title style={styles.titleText}>Welcome to Chat App</Title>
+      <FormInput
+        labelName='Email'
+        value={email}
+        autoCapitalize='none'
+        onChangeText={(userEmail) => setEmail(userEmail)}
+      />
+      <FormInput
+        labelName='Password'
+        value={password}
+        secureTextEntry
+        onChangeText={(userPassword) => setPassword(userPassword)}
+      />
+      <FormButton
+        title='Login'
+        modeValue='contained'
+        labelStyle={styles.loginButtonLabel}
+      />
+      <FormButton
+        title='New user? Join here'
+        modeValue='text'
+        uppercase={false}
+        labelStyle={styles.navButtonText}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  titleText: {
+    fontSize: 22,
+    marginBottom: 10,
+  },
+  loginButtonLabel: {
+    fontSize: 20,
+  },
+  navButtonText: {
+    fontSize: 12,
+  },
+});
