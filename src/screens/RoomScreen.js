444bbaac7a360cc6fb266861bb04b925332f2905
Marc Dos Santos <mdossantosdev@gmail.com>
2020-07-07 13:41:19 +0200

    Update useEffect to Room screen

--- src/screens/RoomScreen.js
+++ src/screens/RoomScreen.js
@@ -8,6 +8,7 @@ import {
 } from '../components/GiftedChat';
 import { AuthContext } from '../contexts/AuthContext';
 import { sendMessage, sendLatestMessage } from '../api/firebaseAPI';
+import { firebase } from '../config/firebase';
 
 export default function RoomScreen({ route }) {
   const [messages, setMessages] = useState([]);
@@ -16,7 +17,37 @@ export default function RoomScreen({ route }) {
   const currentUser = user.toJSON();
 
   useEffect(() => {
-    console.log({ user });
+    const messagesListener = firebase
+      .firestore()
+      .collection('THREADS')
+      .doc(thread._id)
+      .collection('MESSAGES')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((querySnapshot) => {
+        const messages = querySnapshot.docs.map((doc) => {
+          const firebaseData = doc.data();
+
+          const data = {
+            _id: doc.id,
+            text: '',
+            createdAt: new Date().getTime(),
+            ...firebaseData,
+          };
+
+          if (!firebaseData.system) {
+            data.user = {
+              name: firebaseData.user.email,
+              ...firebaseData.user,
+            };
+          }
+
+          return data;
+        });
+
+        setMessages(messages);
+      });
+
+    return () => messagesListener();
   }, []);
 
   const handleSend = (messages) => {
@@ -30,7 +61,7 @@ export default function RoomScreen({ route }) {
     <GiftedChat
       messages={messages}
       onSend={handleSend}
-      user={{ _id: 1, name: 'User Test' }}
+      user={{ _id: currentUser.uid }}
       placeholder='New Message'
       alwaysShowSend
       scrollToBottom
