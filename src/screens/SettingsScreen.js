7c04160d03d0fff4d0ed2479c18aff6214a00d04
Marc Dos Santos <mdossantosdev@gmail.com>
2020-07-10 16:24:36 +0200

    Add List Item with switch for dark theme

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { List, Switch, Divider } from 'react-native-paper';
 
 export default function SettingsScreen() {
   return (
     <View style={styles.container}>
-      <Text>Settings Screen</Text>
+      <List.Item
+        title='Dark theme'
+        left={(props) => <List.Icon {...props} icon='brightness-4' />}
+        right={() => <Switch value={true} />}
+      />
+      <Divider />
     </View>
   );
 }
@@ -12,7 +18,5 @@ export default function SettingsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
