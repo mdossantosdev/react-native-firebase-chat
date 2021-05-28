9dbf22a02e456a806f6db1b887cf63c174b8e684
Marc Dos Santos <mdossantosdev@gmail.com>
2021-05-28 11:44:56 +0200

    Add LocationView component

---
+++ src/components/LocationView/LocationView.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { styles } from './styles';
+
+export const LocationView = ({ location }) => {
+  const LATITUDE_DELTA = 0.01;
+  const LONGITUDE_DELTA = 0.02;
+
+  return (
+    <TouchableOpacity
+      style={styles.container}
+    >
+      <MapView
+        style={styles.mapView}
+        region={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }}
+        scrollEnabled={false}
+        zoomEnabled={false}
+      >
+        <Marker
+          coordinate={{ latitude : location.latitude , longitude : location.longitude }}
+        />
+      </MapView>
+    </TouchableOpacity>
+  );
+};
---
+++ src/components/LocationView/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+import { COLOR_PRIMARY } from '../../constants/Colors';
+
+export const styles = StyleSheet.create({
+  container: {
+    width: 250,
+    height: 250,
+    borderRadius: 20,
+    backgroundColor: COLOR_PRIMARY,
+  },
+  mapView: {
+    width: 250,
+    height: 250,
+    borderRadius: 20,
+  },
+});
