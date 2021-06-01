4a7fe3ef136ab14bf0fe6252c078716907df7ce4
Marc Dos Santos <mdossantosdev@gmail.com>
2021-06-01 14:26:17 +0200

    Add Language screen

--- src/screens/Language/Language.js
+++ src/screens/Language/Language.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, FlatList } from 'react-native';
+import { TouchableOpacity} from 'react-native-gesture-handler';
+import { List, Divider } from 'react-native-paper';
 import { styles } from './styles';
 
+const languages = [
+  { name: 'English' },
+  { name: 'French' },
+  { name: 'Spanish' },
+];
+
 export const Language = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState(null);
+
+  const handleSelectLanguage = (language) => {
+    setSelectedLanguage(language);
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <View>
+        <TouchableOpacity onPress={() => handleSelectLanguage(item.name)}>
+          <List.Item
+            title={item.name}
+            right={(props) => selectedLanguage === item.name ? <List.Icon {...props} icon='check' /> : null}
+          />
+        </TouchableOpacity>
+        <Divider />
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Language Screen</Text>
+      <FlatList
+        data={languages}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+      />
     </View>
   );
 };
