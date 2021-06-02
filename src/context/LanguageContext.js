1db16e092f843e90bf9034f69f82d9cd2496ef62
Marc Dos Santos <mdossantosdev@gmail.com>
2021-06-02 11:04:06 +0200

    Add i18n to LanguageContext

--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -1,9 +1,17 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18next from '../translations/i18n';
 
 export const LanguageContext = createContext();
 
 export const LanguageProvider = ({ children }) => {
   const [selectedLanguage, setSelectedLanguage] = useState(null);
+
+  useEffect(() => {
+    if (selectedLanguage) {
+      i18next.changeLanguage(selectedLanguage);
+    }
+  }, [selectedLanguage]);
 
   const handleSelectLanguage = (language) => {
     setSelectedLanguage(language);
@@ -11,7 +19,9 @@ export const LanguageProvider = ({ children }) => {
 
   return (
     <LanguageContext.Provider value={{ selectedLanguage, handleSelectLanguage }}>
-      {children}
+        <I18nextProvider i18n={i18next}>
+          {children}
+      </I18nextProvider>
     </LanguageContext.Provider>
   );
 };
