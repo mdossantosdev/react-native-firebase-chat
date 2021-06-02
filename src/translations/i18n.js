3124363dbe82e8c90412ff06d6da40b8b6aed2c3
Marc Dos Santos <mdossantosdev@gmail.com>
2021-06-02 10:43:41 +0200

    Add languageDetector to i18n

--- src/translations/i18n.js
+++ src/translations/i18n.js
@@ -1,9 +1,22 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import * as Localization from 'expo-localization';
 
 const resources = {}
 
+const languageDetector = {
+  type: 'languageDetector',
+  async: true,
+  detect: (callback) => {
+    const locale = Localization.locale.split('-')[0];
+    callback(locale);
+  },
+  init: () => {},
+  cacheUserLanguage: () => {},
+};
+
 i18next
+  .use(languageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
